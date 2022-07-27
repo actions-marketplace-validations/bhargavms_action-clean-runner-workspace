a115a311b06ed332d22672766fa9cec15b48e3e1
Bhargav Mogra <bhargav@honestbank.com>
2022-07-27 14:58:44 +0700

    feat: add action to clean workspace

---
+++ clean_workspace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e # fail on error
+
+# include hidden files
+# https://askubuntu.com/questions/740805/how-can-i-remove-all-files-from-current-directory-using-terminal
+shopt -s dotglob
+# shellcheck disable=SC2035
+rm -rf *
